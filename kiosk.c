8447b0f3df7d19af5631c793054282dfb799d314
Karl Sandfort <dev@agarite.com>
2014-10-05 21:54:37 -0400

    kiosk 1.0.2 Added color to box display Update README file

--- kiosk.c
+++ kiosk.c
@@ -3,23 +3,48 @@
 #include <curses.h>
 #include <stdlib.h>
 
+/* colors idefined in /usr/include/ncurses.h
+#define COLOR_BLACK	0
+#define COLOR_RED	1
+#define COLOR_GREEN	2
+#define COLOR_YELLOW	3
+#define COLOR_BLUE	4
+#define COLOR_MAGENTA	5
+#define COLOR_CYAN	6
+#define COLOR_WHITE	7 */
+
 void routines();
 
 int main(int argc, char *argv[])
 {
 	initscr();
+	start_color();
 
 	while (1)
 	{
+		/* This is one way to draw a border */
+		init_pair(1, COLOR_CYAN, COLOR_BLACK);
+		color_set(1, NULL);
 		box(stdscr, ACS_VLINE, ACS_HLINE);
 		refresh();
-		sleep(5);
+		sleep(2);
+
+		/* Clear the screen */
+		clear();
+		refresh();
+		sleep(2);
 	
-		/* border(0x00ba,0x00ba,0x00cd,0x00cd,0x00c9,0x00bb,0x00c8,0x00bc); */
+		/* This is another way to draw a border */
+		init_pair(2, COLOR_MAGENTA, COLOR_BLACK);
+		color_set(2, NULL);
 		border(ACS_VLINE,ACS_VLINE,ACS_HLINE,ACS_HLINE,ACS_ULCORNER,ACS_URCORNER,ACS_LLCORNER,ACS_LRCORNER);
 		refresh();
-		sleep(5);
+		sleep(2);
 	
+		/* Clear the screen */
+		clear();
+		refresh();
+		sleep(2);
 	}
 
 	endwin();

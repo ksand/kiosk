e0e865bc6e13eba8dc5ab5aab7aaeb65872e7848
Karl Sandfort <dev@agarite.com>
2014-10-07 21:51:13 -0400

    kiosk->chart 1.0.3.2 Cycles through character codes for addch(). Unable to print chart of all line-drawing characters. Further investigation can be done on this branch

--- kiosk.c
+++ kiosk.c
@@ -4,19 +4,45 @@
 
 int main(int argc, char *argv[])
 {
-	char c;
+	char c, buf[16];
+	int i, code;
 
 	initscr();
 	start_color();
-	init_pair(1, COLOR_BLUE, COLOR_BLACK);
+	init_pair(1, COLOR_WHITE, COLOR_BLACK);
+	init_pair(2, COLOR_RED, COLOR_BLACK);
 	attrset(COLOR_PAIR(1));
+	code = 32;
 
 	while (1)
 	{
+		while (1)
+		{
+			sprintf(buf, "%4d ", code);
+			addstr(buf);
+			sprintf(buf, "0x%04x ", code);
+			addstr(buf);
+			attron(A_BOLD);
+			addch(code);
+			attroff(A_BOLD);
+			addstr("\n");
+			code ++;
+
+			if (code % 25 == 0)
+				break;
+		}
 		addch(0x00bb);
+		addstr("\n");
+		attrset(COLOR_PAIR(2));
+		addstr("Press q to quit\n");
+		attrset(COLOR_PAIR(1));
 		refresh();
 		c = getchar();
-		break;
+		
+		if (c == 'q' || c == 'Q')
+			break;
+
+		clear();
 	}
 
 	endwin();

9314dd71841bb9340ebcafbb1c4d92ab156ec8e9
Karl Sandfort <dev@agarite.com>
2014-10-11 01:46:48 -0400

    kiosk-noaa 1.0.3.1 Branch from kiosk 1.0.3 that displays NOAA weather reports

--- kiosk.c
+++ kiosk.c
@@ -2,66 +2,137 @@
 
 #include <curses.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/stat.h>
 #include "kiosk.h"
 
-#define DELAY_SECONDS 2
+#define NOAA_URL "http://weather.noaa.gov/pub/data/observations/metar/stations/"
+#define ICAO_CODE "KPOU"
+#define DELAY_SECONDS 600
+#define BUFSIZE 1024
+#define SUCCESS 0
+#define LCOL 3
 
-void genRand(struct windims *wd);
-void initColorPairs();
+void klog(char *info);
 
 int main(int argc, char *argv[])
 {
-	int colorpair, row, col;
+	int row, col, counter;
+	char scode[5];
+	char buf[BUFSIZE];
+	struct stat fs;
 	struct windims wd;
-	WINDOW *win;
+	FILE *rpt;
+	time_t t1, t2;
 	
 	initscr();
 	start_color();
-	initColorPairs();
-
-	colorpair = 1;
+	init_pair(1, COLOR_RED, COLOR_BLACK);
+	init_pair(2, COLOR_GREEN, COLOR_BLACK);
+	init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(4, COLOR_BLUE, COLOR_BLACK);
+	init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
+	init_pair(6, COLOR_CYAN, COLOR_BLACK);
+	init_pair(7, COLOR_WHITE, COLOR_BLACK);
+	counter = 1;
 
 	while (1)
 	{
 		/* Draw a border */
-		color_set(colorpair, NULL);
+		color_set(4, NULL);
 		box(stdscr, ACS_VLINE, ACS_HLINE);
+
+		/* Get the screen dimensions */
 		getmaxyx(stdscr, row, col);
 		wd.maxrow = row;
 		wd.maxcol = col;
+
+		/* Set the display color */
 		color_set(7, NULL);
-		mvprintw(row - 2, 2, "%d rows %d cols", row, col);
+
+		/* Set ICAO code for the weather station */
+		strcpy(scode, ICAO_CODE);
+		mvprintw(1, LCOL, "Station Code: %s", scode);
+		
+		/* Check if the local file exists */
+		sprintf(buf, "%s.TXT", scode);
+		if (stat(buf, &fs) == SUCCESS)
+		{
+			/* Local file exists. Get it's time stamp and log it */
+			t1 = fs.st_mtime;
+			sprintf(buf, "%s.TXT exists with timestamp %lu", scode, t1);
+			mvprintw(2, LCOL, buf);
+			klog(buf);
+			sprintf(buf, "wget -N --dns-timeout=40 --quiet %s%s.TXT", NOAA_URL, scode);
+		}
+		else
+		{
+			/* Create new file */
+			sprintf(buf,  "%s.TXT does not exist... creating new %s.TXT", scode, scode);
+			mvprintw(2, LCOL, buf);
+			klog(buf);
+			sprintf(buf, "wget -S --dns-timeout=40 --quiet %s%s.TXT", NOAA_URL, scode);
+		}
+
+		/* Execute the wget command */
+
+		if (system(buf) ==	SUCCESS)
+		{
+			mvprintw(3, LCOL, "wget succeeded %d", counter);
+			counter++;
+			sprintf(buf, "%s.TXT", scode);
+			
+			if (stat(buf, &fs) == SUCCESS)
+			{
+				t2 = fs.st_mtime;
+				
+				if (t1 != t2)
+					sprintf(buf, "%s.TXT exists with new timestamp %lu", scode, t2);
+
+				klog(buf);
+				sprintf(buf, "%s.TXT", scode);
+				rpt = fopen(buf, "r");
+
+				if (rpt != NULL)
+				{
+					row = 4;
+					color_set(2, NULL);
+					while (fgets(buf, BUFSIZE, rpt) != NULL)
+					{
+						buf[wd.maxcol - LCOL - 4] = '\0';
+						mvprintw(row, LCOL, buf);
+						row++;
+					}
+					color_set(7, NULL);
+					fclose(rpt);
+				}
+				else
+				{
+					mvprintw(4, LCOL, "Unable to open file");
+				}
+			}
+			else
+			{
+				sprintf(buf, "%s.TXT does not exist", scode);
+				klog(buf);
+				mvprintw(4, LCOL, buf);
+			}
+		}
+		else
+		{
+			sprintf(buf, "wget failed: %s", buf);
+			mvprintw(3, LCOL, buf);
+			klog(buf);
+		}
+		
 		refresh();
 		sleep(DELAY_SECONDS);
-		
-		/* Cycle through the colors 1-7 */
-		colorpair++;
-		
-		if (colorpair > 7)
-			colorpair = 1;
-		
-		/* Generate random window specs */
-		genRand(&wd);
-
-		/* Display the window dimensions on the bottom row */
-		color_set(7, NULL);
-		mvprintw(row - 2, 2, "row=%d col=%d height=%d width=%d", wd.row, wd.col, wd.height, wd.width);
-
-		/* Create a new window */
-		win = newwin(wd.height, wd.width, wd.row, wd.col);
-		wbkgdset(win, COLOR_PAIR(colorpair));
-		box(win, 0, 0);
-		wrefresh(win);
-		refresh();
-		sleep(DELAY_SECONDS);
-
-		/* Destroy the window */
-		wborder(win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
-		wrefresh(win);
-		delwin(win);
+	
+		/* Clear the screen and wait */
 		clear();
 		refresh();
-		sleep(DELAY_SECONDS);
+		sleep(30);
 	}
 
 	endwin();
--- routines.c
+++ routines.c
@@ -1,132 +1,18 @@
 /* routines.c */
 
-#include <curses.h>
-#include "kiosk.h"
+#include <time.h>
+#include <stdio.h>
 
-#define DIVISOR 10000000
-#define TRYLIMIT 10000
-
-void initColorPairs()
+void klog(char *info)
 {
-	init_pair(1, COLOR_RED, COLOR_BLACK);
-	init_pair(2, COLOR_GREEN, COLOR_BLACK);
-	init_pair(3, COLOR_YELLOW, COLOR_BLACK);
-	init_pair(4, COLOR_BLUE, COLOR_BLACK);
-	init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
-	init_pair(6, COLOR_CYAN, COLOR_BLACK);
-	init_pair(7, COLOR_WHITE, COLOR_BLACK);
-	return;
+	time_t tlog;
+	struct tm t, *logtime;
+	FILE *log;
+
+	time(&tlog);
+	logtime = localtime_r(&tlog, &t);
+	log = fopen("logfile", "a");
+	fprintf(log, "[%d-%02d-%02d %02d:%02d:%02d] %s\n", 1900 + logtime->tm_year, logtime->tm_mon, logtime->tm_mday, logtime->tm_hour, logtime->tm_min, logtime->tm_sec, info);
+	fclose(log);
 }
 
-/*
- * genRand()
- *
- * Generates the dimensions and position of a window.
- * */
-
-void genRand(struct windims *wd)
-{
-	int num, min, max, counter;
-
-	/* Generate random height. It has to be at least 3 rows high
-	 * and has to fit within the border and leave a row at the
-	 * bottom to print a status line.
-	 * */
-	min = 3;
-	max = wd->maxrow - 3;
-	counter = 0;
-	while (1)
-	{
-		counter++;
-		num = rand();
-		num /= DIVISOR;
-
-		if (num >= min && num <= max)
-		{
-			wd->height = num;
-			break;
-		}
-
-		if (counter > TRYLIMIT)
-		{
-			wd->height = max;
-			break;
-		}
-	}
-
-	/* Generate random row. The row must allow the window to
-	 * fit on the screen, leaving a row at the bottom
-	 * to print a status line.
-	 * */
-	min = 2;
-	max = wd->maxrow - wd->height - 3;
-	counter = 0;
-	while (1)
-	{
-		counter++;
-		num = rand();
-		num /= DIVISOR;
-
-		if (num >= min && num <= max)
-		{
-			wd->row = num;
-			break;
-		}
-
-		if (counter > TRYLIMIT)
-		{
-			wd->row = min;
-			break;
-		}
-	}
-
-	/* Generate random width. It has to be at least 3 columns wide
-	 * and has to fit within the border
-	 * */
-	min = 3;
-	max = wd->maxcol - 2;
-	counter = 0;
-	while (1)
-	{
-		counter++;
-		num = rand();
-		num /= DIVISOR;
-
-		if (num >= min && num <= max)
-		{
-			wd->width = num;
-			break;
-		}
-
-		if (counter > TRYLIMIT)
-		{
-			wd->width = max;
-			break;
-		}
-	}
-	
-	/* Generate random column. The column must allow the window to
-	 * fit on between the borders.
-	 * */
-	min = 1;
-	max = wd->maxcol - wd->width - 2;
-	counter = 0;
-	while (1)
-	{
-		counter++;
-		num = rand();
-		num /= DIVISOR;
-
-		if (num >= min && num <= max)
-		{
-			wd->col = num;
-			break;
-		}
-
-		if (counter > TRYLIMIT)
-		{
-			wd->col = min;
-			break;
-		}
-	}
-}
